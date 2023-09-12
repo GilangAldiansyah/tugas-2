fc4ba80682e2474fea7da3faffbddd3edb239911
Gilang Aldiansyah <95297959+GilangAldiansyah@users.noreply.github.com>
2023-09-12 10:35:17 +0700

    tugas2

--- lib/ui/tampil_data.dart
+++ lib/ui/tampil_data.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 
 class TampilData extends StatelessWidget {
-  final String data; //contoh data yang di-passing
-  
-  const TampilData({Key? key, required this.data}) : super(key: key);
+  final String nama; //contoh data yang di-passing
+  final String nim; //contoh data yang di-passing
+  final int tahunLahir; //contoh data yang di-passing
+  final int usia;
+
+  TampilData({
+    Key? key,
+    required this.nama,
+    required this.nim,
+    required this.tahunLahir,
+  })  : usia = DateTime.now().year - tahunLahir,
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +20,8 @@ class TampilData extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Perkenalan"),
       ),
+      body: Container(
+          child: Text("Nama Saya $nama, NIM $nim, dan umur saya adalah $usia")),
     );
   }
 }
